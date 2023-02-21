9ae9b980d23a12ec7fac8c88b0c53f8f80cb8819
Jason Conaway <conaway4893@gmail.com>
2023-02-21 18:14:21 -0500

    Extracted CaveLights class out of CaveWalls

---
+++ Assets/Scripts/CaveLights.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameU
+{
+    public class CaveLights : MonoBehaviour
+    {
+        [SerializeField] Light lightPrefab;
+
+        private CaveSystem caves;
+        private CaveWalls caveWalls;
+
+        private void Awake()
+        {
+            caves = GetComponent<CaveSystem>();
+            caveWalls = GetComponent<CaveWalls>();
+        }
+
+        private void Start()
+        {
+            caves.OnCreated += Caves_OnCreated;
+        }
+
+        private void Caves_OnCreated()
+        {
+            Illuminate();
+        }
+
+        private void Illuminate()
+        {
+            for (int roomIndex = 0; roomIndex < caves.RoomCount; roomIndex++)
+            {
+                Vector3Int roomCoordinates = caves.GetRoomCenter(roomIndex);
+                Vector3Int coordinates = roomCoordinates.Step(Direction.Up);
+                Vector3 position = caveWalls.GetCellPosition(coordinates);
+                Light light = Instantiate(lightPrefab, position, Quaternion.identity);
+                light.transform.parent = transform;
+                light.name = $"Light {coordinates}";
+            }
+        }
+    }
+}
